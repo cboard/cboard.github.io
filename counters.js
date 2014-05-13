2be4d95fdb381c6f9521aee43fa91104807c63fe
magicchalkboard <mchalkboard@gmail.com>
2014-05-13 21:06:15 +0400

    Fucking git add

---
+++ counters.js
@@ -0,0 +1,45 @@
+(function (d, w, c) {
+  (w[c] = w[c] || []).push(function() {
+    try {
+      w.yaCounter24950972 = new Ya.Metrika({id:24950972,
+        webvisor:true,
+      clickmap:true,
+      trackLinks:true,
+      accurateTrackBounce:true});
+    } catch(e) { }
+  });
+
+  var n = d.getElementsByTagName("script")[0],
+s = d.createElement("script"),
+f = function () { n.parentNode.insertBefore(s, n); };
+s.type = "text/javascript";
+s.async = true;
+s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+if (w.opera == "[object Opera]") {
+  d.addEventListener("DOMContentLoaded", f, false);
+} else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-50762701-1', 'melovaya-doska.ru');
+ga('send', 'pageview');
+
+(window.Image ? (new Image()) : document.createElement('img')).src = location.protocol + '//vk.com/rtrg?r=U/dtK8ymrwPYmezzt8UolQMQCdYzHeZKxCtVNdRUGEKVosQd6J4SM6p/HAaqkT8pXODUBFMXbCm/t2QtiT266IB0srAReCqrLsFO2brY3UQSYbdvOmNhWcGaAsMffzgcjauVnJnfuYyr/w5giiphTGwIvXHAwY7l63Ims3q2Dt0-';
+
+var _oms = window._oms || [];
+
+_oms.push(["set_project_id", "gsuduwiufnmkwnlnollnflkddpmcueilescusvra"]);
+_oms.push(["set_domain", ".melovaya-doska.ru"]);
+(function() {
+  var oms = document.createElement("script");
+  oms.type = "text/javascript";
+  oms.async = true;
+  oms.src = "//ohmystats.com/oms.js";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(oms, s);
+})();
